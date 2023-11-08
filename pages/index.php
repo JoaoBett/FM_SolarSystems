998a2b294a277df59a7d366e0be946f9ea1d8584
Joao Bett <joao.gouveia.bettencourt@gmail.com>
2023-11-08 12:14:26 +0000

    feat: html structure

---
+++ pages/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en-pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="authors" content="João Bettencourt">
+    <title>FM Solar Systems</title>
+    <link rel="icon" href="../assets/images/logo.png">
+    <link rel="stylesheet" href="../assets/style.css">
+</head>
+<body>
+    
+</body>
+</html>
